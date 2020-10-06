a5b90603accc96394bd36f423c4b81976f5924fd
swapnakharat <49679833+swapnakharat@users.noreply.github.com>
2020-10-06 11:56:35 +0530

    Update DropDownHandling.java

--- src/Practice/DropDownHandling.java
+++ src/Practice/DropDownHandling.java
@@ -2,7 +2,10 @@ package Practice;
 //by using select class ,every  select tag will have option child tags,and every option tag will have value attribute
 public class DropDownHandling
 {
-	
+	public void m1()
+	{
+		system.out.println("Hiii");
+	}
 
 	
 }
