be185ab68bd8b768c150dc83ee009cf9f1da77f2
swapna kharat <swapnakharat18@gmail.com>
2020-10-06 11:47:31 +0530

    My first commit

--- src/Practice/AlertHandle.java
+++ src/Practice/AlertHandle.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 // alert can be handle using alert interface and in this we use some methods like void dismiss(),void accept(), String getText(),void sendKeys(String stringToSend)
 public class AlertHandle
 {
+	// commit changes
 	public static void main(String[] args) throws Exception
 	{
 		System.setProperty("webdriver.chrome.driver", "E:\\Testing Shastra\\All jar File\\chromedriver.exe");
